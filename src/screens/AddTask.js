cbad341eb6135806f745aa68c1fa8993c77d75de
Guilherme <guilherme.pomp@gmail.com>
2020-08-07 21:29:13 -0300

    Data no modal

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -6,16 +6,42 @@ import {
     StyleSheet,
     TouchableOpacity,
     TextInput,
-    TouchableWithoutFeedback
+    TouchableWithoutFeedback,
+    Platform
 } from 'react-native'
 import commonStyles from '../commonStyles'
+import DateTimePicker from '@react-native-community/datetimepicker'
+import moment from 'moment'
 
-const initialState = { desc: '' }
+const initialState = { desc: '', date: new Date(), showDatePicker: false }
 
 export default class AddTask extends Component {
     
     state = {
         ...initialState
+    }
+
+    getDatePicker = () => {
+        let datePicker = <DateTimePicker value={this.state.date} 
+                    onChange={(_, date) => this.setState({ date, showDatePicker: false })} 
+                    mode='date' />
+
+        const dateString = moment(this.state.date).format('dddd, D [de] MMMM [de] YYYY')
+
+        if(Platform.OS === 'android') {
+            datePicker = (
+                <View>
+                    <TouchableOpacity onPress={() => this.setState({ showDatePicker: true })}>
+                        <Text style={styles.date}>
+                            {dateString}
+                        </Text>
+                    </TouchableOpacity>
+                    {this.state.showDatePicker && datePicker}
+                </View>
+            )
+        }
+
+        return datePicker;
     }
 
     render() {
@@ -33,6 +59,7 @@ export default class AddTask extends Component {
                         placeholder="Descrição"
                         onChangeText={desc => this.setState({ desc })}
                         value={this.state.desc} />
+                    {this.getDatePicker()}
                     <View style={styles.buttons}>
                         <TouchableOpacity>
                             <Text style={styles.button}>Salvar</Text>
@@ -84,5 +111,10 @@ const styles = StyleSheet.create({
         margin: 20,
         marginRight: 30,
         color: commonStyles.colors.today
+    },
+    date: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 20,
+        marginLeft: 15
     }
 })
